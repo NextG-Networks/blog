7dd31077badea887c0d626b13bfd10d6aa2fbb7b
Wilhelm Rauston <wilhelm.rauston@gmail.com>
2025-11-17 19:27:36 +0100

    fix

--- nextjs-hybridai-nextg/src/content/topics/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all topic content files
-export { landscapeOfChoice } from './landscape-of-choice';
-export { paradoxOfAgency } from './paradox-of-agency';
-export { liberationFromRegret } from './liberation-from-regret';
-export { mappingCausalFactors } from './mapping-causal-factors';
-export { reframingUncertainty } from './reframing-uncertainty';
-export { adaptingToChange } from './adapting-to-change';
-
